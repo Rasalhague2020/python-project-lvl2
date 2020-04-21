923c80a02677b7d1e9533e0482b721208fa7ac71
Yury Silushkin <yusilushkin@yandex.ru>
2020-04-21 16:15:56 +0300

    Indenteds for linter

--- gendiff/test_gendiff.py
+++ gendiff/test_gendiff.py
@@ -3,6 +3,6 @@ from gendiff import engine
 
 def test_diff():
     with open('./gendiff/tests/fixtures/correct_out1.txt', 'r') as f:
-        assert (f.read() == 
-               engine.generate_diff('./gendiff/tests/fixtures/before.json',
-                                    './gendiff/tests/fixtures/after.json'))
+        assert (f.read() ==
+                engine.generate_diff('./gendiff/tests/fixtures/before.json',
+                                     './gendiff/tests/fixtures/after.json'))
