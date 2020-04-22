9be311ac53e1896b49a74c60152fcf33ac1ce790
Yury Silushkin <yusilushkin@yandex.ru>
2020-04-22 12:25:33 +0300

    Add tests for yaml files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,7 +2,11 @@ from gendiff import generate_diff
 
 
 def test_diff():
-    # with open('./tests/fixtures/correct_out1.txt', 'r') as f:
-    #     assert (f.read() ==
-    #             generate_diff('./tests/fixtures/before.json',
-    #                           './tests/fixtures/after.json'))
+    with open('./tests/fixtures/correct_out1.txt', 'r') as f:
+        correct_out = f.read()
+    assert (correct_out ==
+            generate_diff('./tests/fixtures/before.json',
+                          './tests/fixtures/after.json'))
+    assert (correct_out ==
+            generate_diff('./tests/fixtures/before.yml',
+                          './tests/fixtures/after.yml'))
